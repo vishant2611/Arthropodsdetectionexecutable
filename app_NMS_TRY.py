c99d8541be238780f1f433149f807c843b8f2bef
vishant2611 <aryan.bhatia26@gmail.com>
2024-05-09 11:53:53 -0400

    Updated with Firebase

--- app_NMS_TRY.py
+++ app_NMS_TRY.py
@@ -1,3 +1,4 @@
+import pyrebase
 import streamlit as st
 import time 
 import cv2
@@ -12,7 +13,11 @@ from sahi.utils.yolov8 import download_yolov8n_model
 from sahi import AutoDetectionModel
 from sahi.utils.cv import read_image
 from sahi.predict import get_prediction, get_sliced_prediction, predict, visualize_object_predictions
-#from IPython.display import Image as IPythonImage
+from IPython.display import Image as IPythonImage
+import urllib.request
+import numpy as np
+
+
 
 st.set_page_config(layout="wide", page_title="Arthropods Detection APP")
 
@@ -21,15 +26,14 @@ st.write("Try uploading an image to detect the arthropods. Full quality images c
 st.sidebar.write("## Upload :gear:")
 
 
-#NMS function -  selecting the most confident bounding box detections and eliminating overlapping boxes that are less confident, 
-    #based on the provided IoU threshold, thus reducing the number of redundant boxes for the same object.
+# first write the file name u want to display on database and file which u want to upload
+#storage.child("PXL_20240202_184304751.jpg").put("/content/drive/MyDrive/PXL_20240202_184304751.jpg")
 
-#sort scores in descending, select box with highest score, calculate IOU(measure of overlap betwen 2 box), 
-    # removes box with high IOU i.e if selected box is > than certain threshold, it has high overlap & box is removed
-        # from consideration & repeats until there are no more box to consider.
+#first write the file name u want to download then write the local name
+#storage.download("PXL_20240202_184304751.jpg","downloaded.jpg")
 
-# function looks at bunch of boxes, picks best one & removes any other boxes that overlap too much with it.
-    # result is list of best box, name of object they contain and how confident function is that object is really.
+#*************NMS Function starts*******************************
+
 def nms(boxes, scores, iou_threshold, object_names):
     sorted_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)
     selected_indices = []
@@ -50,7 +54,6 @@ def nms(boxes, scores, iou_threshold, object_names):
 
     return selected_boxes, selected_object_names, selected_confidence_scores
 
-#calculates the Intersection over Union (IoU), which is a measure of the overlap between two bounding boxes
 def iou(boxA, boxB):
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
@@ -64,28 +67,41 @@ def iou(boxA, boxB):
     iou = intersection_area / float(boxA_area + boxB_area - intersection_area)
     return iou
 
+
+# ************************Main function starts************************* 
 def process_image(image_file, confidence_threshold):
-    st.write("Hello")   
-    # Define the directory to save the uploaded image
-    save_dir = 'https://github.com/vishant2611/Arthropodsdetectionexecutable' 
-   
-    # Save the uploaded image to the specified directory
-    st.write(image_file.name)
-    saved_image_path = os.path.join(save_dir, image_file.name)
-    st.write(saved_image_path)
-    with open(saved_image_path, 'wb') as f:
-        f.write(image_file.getbuffer())
-    
-    # Now, you can use saved_image_path for further processing
-    detection_model = AutoDetectionModel.from_pretrained(
+  st.write(image_file.name)
+ 
+  config = {
+  "apiKey": "AIzaSyAJzChgGmAHIcz0Jo7zxXsZ75sP0zoB_3c",
+  "authDomain": "arthropods-fd246.firebaseapp.com",
+  "projectId": "arthropods-fd246",
+  "storageBucket": "arthropods-fd246.appspot.com",
+  "messagingSenderId": "335666008622",
+  "appId": "1:335666008622:web:7ebe04ff7f98e8771d85bf",
+  "measurementId": "G-BM9VQ53BRH",
+  "serviceAccount": "/content/drive/MyDrive/serviceAccount.json",
+  "databaseURL": "https://arthropods-fd246-default-rtdb.firebaseio.com/"};
+
+  # ***********Storing image***********************************
+  firebase = pyrebase.initialize_app(config)
+  storage = firebase.storage()
+  image_bytes = image_file.read()
+  storage.child(image_file.name).put(image_bytes)
+  #***************Fetching image************************************
+  image_url = storage.child(image_file.name).get_url(None)
+  model_url = storage.child("best.pt").get_url(None)
+
+   # ************Now, you can use Image for further processing using SAHI******
+  detection_model = AutoDetectionModel.from_pretrained(
         model_type='yolov8',
-        model_path='/content/drive/MyDrive/Capstone_new/runs/detect/YOLOv8nwithP2/weights/best.pt',
+        model_path=model_url,
         confidence_threshold=confidence_threshold,
         device='cpu'
     )
 
-    result = get_sliced_prediction(
-        saved_image_path,
+  result = get_sliced_prediction(
+        image_url,
         detection_model=detection_model,
         slice_height=640,
         slice_width=640,
@@ -93,89 +109,87 @@ def process_image(image_file, confidence_threshold):
         overlap_width_ratio=0.3
     )
 
-    # reads an image, applies Non-Maximum Suppression to filter the object detections,
-        # and then draws red bounding boxes with a thickness of 2 pixels around the detected objects on the image.
-    img = cv2.imread(saved_image_path, cv2.IMREAD_UNCHANGED)
-    img_converted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    numpydata = asarray(img_converted)
-    boxes = [[pred.bbox.minx, pred.bbox.miny, pred.bbox.maxx, pred.bbox.maxy] for pred in result.object_prediction_list]
-    scores = [pred.score.value for pred in result.object_prediction_list]
-    names  = [pred.category.name for pred in result.object_prediction_list]
-    #iou_threshold = 0.5
-    selected_boxes, selected_names, selected_scores = nms(boxes, scores, iou_threshold,names)
-    image = cv2.imread(saved_image_path)
-    for box, object_name, confidence in zip(selected_boxes, selected_names, selected_scores):
-      x_min, y_min, x_max, y_max = map(int, box)  # Convert coordinates to integers
-      cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)  # Green rectangle with thickness 2
+  boxes = [[pred.bbox.minx, pred.bbox.miny, pred.bbox.maxx, pred.bbox.maxy] for pred in result.object_prediction_list]
+  scores = [pred.score.value for pred in result.object_prediction_list]
+  names  = [pred.category.name for pred in result.object_prediction_list]
+  selected_boxes, selected_names, selected_scores = nms(boxes, scores, iou_threshold,names)
+  
+  
+  # Fetch the image from Firebase Storage
+  image_bytes = urllib.request.urlopen(image_url).read()
+  nparr = np.frombuffer(image_bytes, np.uint8)
+  image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-      # Increase font size by changing the fontScale parameter
-      font_scale = 2.0  # Change this value to adjust font size
-      text = f"{object_name}: {confidence:.2f}"
-      cv2.putText(image, text, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), 2)
+  # Draw bounding boxes and labels on the image
+  for box, object_name, confidence in zip(selected_boxes, selected_names, selected_scores):
+    x_min, y_min, x_max, y_max = map(int, box)
+    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+    font_scale = 2.0
+    text = f"{object_name}: {confidence:.2f}"
+    cv2.putText(image, text, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), 2)
     
-    # for box, object_name, confidence in zip(selected_boxes, selected_names, selected_scores):
-    #   x_min, y_min, x_max, y_max = map(int, box)  # Convert coordinates to integers
-    #   cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)  # Green rectangle with thickness 2
-      # Display object name and confidence score
-      # text = f"{object_name}: {confidence:.2f}"
-      # cv2.putText(image, text, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.imwrite('/content/drive/MyDrive/Capstone_new/result_with_nms.jpg', image)
+  # Encode the result image as bytes
+  _, result_image_bytes = cv2.imencode('.jpg', image)
 
-    # visualize_object_predictions(
-    #     numpydata,
-    #     object_prediction_list=result.object_prediction_list,
-    #     output_dir='/content/drive/MyDrive/Capstone_new',
-    #     file_name='result_701',
-    #     export_format='jpeg'
-    # )
-    col1.write("Original Image :camera:")
-    col1.image(saved_image_path)
-    result_image_path = '/content/drive/MyDrive/Capstone_new/result_with_nms.jpg'
-    result_image = PILImage.open(result_image_path)
-    # col2.write("Detected Image :beetle:")
-    # col2.image(result_image)
-    #st.image(result_image, caption='Result')
-    object_prediction_list = result.object_prediction_list
-    class_map = {
+  # Upload the result image bytes to Firebase Storage
+  storage.child("result_with_nms.jpg").put(result_image_bytes.tobytes())
+
+  # Get the URL of the result image
+  result_image_url = storage.child("result_with_nms.jpg").get_url(None)
+  
+  #Table creation
+  object_prediction_list = result.object_prediction_list
+  class_map = {
     'MA': 'Melon Aphid',
     'NT': 'Nesidiocorius Tenius',
     'OI': 'Orius Insidiosus',
     'WFT': 'Western Flower Thrips',
     'WF': 'White Fly',
     'TS': 'Two Spotted Spidermite'}
-    class_counts = {class_name: 0 for class_name in class_map.keys()}
-    for prediction in object_prediction_list:
-      class_name = prediction.category.name
-      if class_name in class_counts:
-        class_counts[class_name] += 1
+  class_counts = {class_name: 0 for class_name in class_map.keys()}
+  for prediction in object_prediction_list:
+    class_name = prediction.category.name
+    if class_name in class_counts:
+      class_counts[class_name] += 1
       
-    data = {
+  data = {
     'Abbreviation': [],
     'Full Name': [],
     'Count': []}
 
-    for class_name, count in class_counts.items():
-      data['Abbreviation'].append(class_name)
-      data['Full Name'].append(class_map.get(class_name, class_name))
-      data['Count'].append(count)
+  for class_name, count in class_counts.items():
+    data['Abbreviation'].append(class_name)
+    data['Full Name'].append(class_map.get(class_name, class_name))
+    data['Count'].append(count)
 
-    df = pd.DataFrame(data)
+  df = pd.DataFrame(data)
+  table_data = {"Abbreviation": df['Abbreviation'].tolist(), "Full Name": df['Full Name'].tolist(), "Count": df['Count'].tolist()}
     
-    # Display class names and counts as table
-    #table_data = {"Class Name": list(class_counts.keys()), "Count": list(class_counts.values())}
-    table_data = {"Abbreviation": df['Abbreviation'].tolist(), "Full Name": df['Full Name'].tolist(), "Count": df['Count'].tolist()}
-    col2.write("## Class Counts")
-    # col2.table(table_data)
-    col2.write(df.set_index('Abbreviation', drop=True))
-    col3.write("Detected Image :beetle:")
-    col3.image(result_image)
+    
+  #Display of images and table
+  col1.write("Original Image :camera:")
+  col1.image(image_url)
+  col2.write("## Class Counts")
+  col2.write(df.set_index('Abbreviation', drop=True))
+  col3.write("Detected Image")
+  col3.image(result_image_url)
 
+
+
+
+#************Main Funtion Ends************************************
+
+#***************Buttons and sliders initiated***********************
 MAX_FILE_SIZE = 5 * 1024 * 1024 
 col1, col2, col3 = st.columns(3)
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 confidence_threshold = st.sidebar.slider("Confidence Threshold", 0.05, 0.95, 0.05, 0.05,key="confidence_slider")
 iou_threshold = st.sidebar.slider("IoU Threshold", 0.1, 0.9, 0.1, 0.1, key="iou_slider")
+
+
+# ************************Condition start******************************
+
 
 if st.button('Detect Arthropods'):
     
@@ -242,9 +256,3 @@ if (confidence_threshold > 0.05 and confidence_threshold < 1.00) and (iou_thresh
                   time.sleep(3)
                   bar.progress(100)
                   st.success('Success message')
-    # else:
-    #   st.write("In else")
-    #   st.error('Please upload an image')
-
-
-
